4bc90d4e09d491061e9edf28a93e5884ae4df89c
up211 <kristianapetkova98@gmail.com>
2020-02-25 14:46:43 +0200

    opa

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public static void main(String args[]){
 
     System.out.println("Boklik");
     System.out.println("opa");
-    System.out.println("kkjjjk")
+    System.out.println("kkjjjk");
+    public int a;
 }
 }
